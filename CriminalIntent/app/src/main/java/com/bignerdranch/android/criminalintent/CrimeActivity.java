058ec0185c2fe70579640d105bd427cd5007b5c3
YOlandaMcNeill <mcneilly3281@student.faytechcc.edu>
2018-11-06 14:57:08 -0500

    an abstracct activity class

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,13 +1,13 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
